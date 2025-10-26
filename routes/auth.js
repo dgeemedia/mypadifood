3a9ff62733d0cdf940b6ee7b642adfa002d161ba
dgeemedia <lolumah@byupathway.edu>
2025-10-26 14:33:21 +0100

    ok

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ router.get('/login', authController.showLogin);
 router.post('/login', authController.login);
 
 // signup choice page
+router.get('/signup-choice', authController.showSignupChoice);
 router.get('/signup', authController.showSignupChoice);
 
 // Add both POST and GET for /logout (POST recommended)
